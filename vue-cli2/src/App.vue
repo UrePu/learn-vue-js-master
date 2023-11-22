e42597914e3f3ee773ef2eafb81e332bee19cad5
Your Name <you@example.com>
2023-11-22 10:47:15 +0900

    last

---
+++ vue-cli2/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <app-header
+     v-bind:propsdata="str"
+     v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+  import AppHeader from './components/AppHeader.vue'; 
+export default {
+  data:function() {
+    return{
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header' : AppHeader
+  },
+  methods :{
+    renewStr: function() {
+      this.str = "Helllllo"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
